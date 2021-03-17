1f211f1b2cdf3c3c8601262517a1cc727da51dcd
Parsa Jahanlou <pjahanlou@gmail.com>
2021-03-16 20:29:57 -0500

    Added a more user-friendly input taking system and the pattern matching algorithms

--- CSC 325/Program 1/Node.py
+++ CSC 325/Program 1/Node.py
@@ -1,24 +1,4 @@
-# I got all of this code from
-# github.com/OmkarPathak/Data-Structures-using-Python/blob/master/Linked%20Lists/SinglyLinkedList.py
-
-class Node(object):
-    # Each node has its data and a pointer that points to next node in the Linked List
-    def __init__(self, data, next = None):
+class Node:
+    def __init__(self, data):
         self.data = data
-        self.next = next
-
-    # function to set data
-    def setData(self, data):
-        self.data = data
-
-    # function to get data of a particular node
-    def getData(self):
-        return self.data
-
-    # function to set next node
-    def setNext(self, next):
-        self.next = next
-
-    # function to get the next node
-    def getNext(self):
-        return self.next
+        self.next = None
